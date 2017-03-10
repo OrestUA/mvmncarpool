bffc680f8217994c400cba510f2450d1872f9398
Mykola Makhin <Mykola.Makhin@gmail.com>
2017-03-10 12:53:47 +0200

    Draft email/password validations.

--- src/main/java/x/mvmn/carpool/web/ctrl/UserController.java
+++ src/main/java/x/mvmn/carpool/web/ctrl/UserController.java
@@ -1,7 +1,9 @@
 package x.mvmn.carpool.web.ctrl;
 
 import java.util.Locale;
+import java.util.regex.Pattern;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.validator.constraints.Email;
@@ -16,12 +18,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.savoirtech.logging.slf4j.json.LoggerFactory;
+import com.savoirtech.logging.slf4j.json.logger.Logger;
+
 import x.mvmn.carpool.model.User;
 import x.mvmn.carpool.service.UserConfirmationService;
 import x.mvmn.carpool.service.persistence.UserRepository;
 
 @RestController
 public class UserController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	UserRepository userRepository;
@@ -35,14 +42,44 @@ public class UserController {
 	@Value("${mvmncarpool.user.confirmation.required:true}")
 	boolean confirmationRequired;
 
-	@RequestMapping(path = "/check_email_available")
-	public @ResponseBody boolean checkEmailAvailability(@RequestParam("email") String emailAddress) {
-		return userRepository.findByEmailAddress(emailAddress) == null;
+	@Value("${mvmncarpool.emailregex:^.*@.*$}")
+	String emailRegExPatternStr;
+
+	@Value("${mvmncarpool.password_policy.length.min:6}")
+	int passwordLengthMin;
+
+	@Value("${mvmncarpool.password_policy.length.max:256}")
+	int passwordLengthMax;
+
+	@Value("${mvmncarpool.password_policy.regex:^.*$}")
+	String passwordRegExPatternStr;
+
+	protected Pattern emailRegEx;
+	protected Pattern passwordRegEx;
+
+	@PostConstruct
+	protected void compileRegexps() {
+		this.emailRegEx = Pattern.compile(emailRegExPatternStr);
+		this.passwordRegEx = Pattern.compile(passwordRegExPatternStr);
+	}
+
+	public static enum EmailCheckResult {
+		INVALID, TAKEN, OK
+	}
+
+	@RequestMapping(path = "/check_email", method = RequestMethod.POST)
+	public @ResponseBody EmailCheckResult checkEmail(@RequestParam("email") String emailAddress) {
+		if (!isEmailValid(emailAddress)) {
+			return EmailCheckResult.INVALID;
+		}
+		if (!isEmailAvailable(emailAddress)) {
+			return EmailCheckResult.TAKEN;
+		}
+		return EmailCheckResult.OK;
 	}
 
 	@RequestMapping(path = "/reset_password", method = RequestMethod.POST)
 	public @ResponseBody String resetPassword(@Email @RequestParam("email") String emailAddress, Locale locale, HttpServletResponse response) {
-
 		String result;
 		User user = userRepository.findByEmailAddress(emailAddress);
 		if (user != null) {
@@ -66,9 +103,8 @@ public class UserController {
 	public @ResponseBody String doRegister(@Email @RequestParam("email") String emailAddress, @RequestParam("password") String password,
 			@RequestParam("passwordConfirmation") String passwordConfirmation, Locale locale, HttpServletResponse response) {
 		String result; // TODO: JSON object result
-		// TODO: Configurable password validation (min length and characters)
-		// TODO: Validate email/username
-		if (password != null && password.length() > 8 && password.equals(passwordConfirmation)) {
+		if (isEmailValid(emailAddress) && isEmailAvailable(emailAddress) && password != null && password.equals(passwordConfirmation)
+				&& isPasswordValid(passwordConfirmation)) {
 			User user = null;
 			try {
 				user = new User();
@@ -83,8 +119,11 @@ public class UserController {
 				userRepository.save(user);
 				result = "ok";
 			} catch (DataIntegrityViolationException ex) {
-				// TODO: re-check email availability
-				throw new RuntimeException("Email already taken");
+				if (!isEmailAvailable(emailAddress)) {
+					throw new RuntimeException("Email already taken");
+				} else {
+					throw ex;
+				}
 			} catch (Exception e) {
 				// TODO: better handling
 				try {
@@ -92,6 +131,7 @@ public class UserController {
 						userRepository.delete(user);
 					}
 				} catch (Exception cleanupUserException) {
+					LOGGER.warn().message("Failed to delete user after reg confirm sending failed").exception("exception", cleanupUserException).log();
 					cleanupUserException.printStackTrace();
 				}
 				throw new RuntimeException(e);
@@ -109,16 +149,32 @@ public class UserController {
 			@RequestParam("passwordConfirmation") String passwordConfirmation, HttpServletResponse response, Model model) {
 		String result;
 		User user = userConfirmationService.validatePasswordResetRequest(emailAddress, confirmationId);
-		// TODO: password validation in one place
-		if (user != null && password != null && password.length() > 8 && password.equals(passwordConfirmation)) {
+		if (user != null && password != null && password.equals(passwordConfirmation) && isPasswordValid(password)) {
 			user.setPassword(passwordEncoder.encode(password));
 			user.setPasswordResetRequestId(null);
 			user.setPasswordResetRequestUnixTime(0);
 			userRepository.save(user);
 			result = "ok";
 		} else {
+			// TODO: specific errors
 			result = "error";
 		}
 		return result;
+	}
+
+	protected boolean isEmailAvailable(String emailAddress) {
+		// TODO: move to service/helper
+		return userRepository.findByEmailAddress(emailAddress) == null;
+	}
+
+	protected boolean isEmailValid(String email) {
+		// TODO: move to service/helper
+		return emailRegEx.matcher(email).matches();
+	}
+
+	protected boolean isPasswordValid(String password) {
+		// TODO: move to service/helper
+		int pwdLength = password.length();
+		return pwdLength >= passwordLengthMin && pwdLength <= passwordLengthMax && passwordRegEx.matcher(password).matches();
 	}
 }
