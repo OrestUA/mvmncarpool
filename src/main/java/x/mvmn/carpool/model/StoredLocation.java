cb1b6fbbe503957998caa424965703c6b6fe9b77
Mykola Makhin <mvmn.inbox@gmail.com>
2017-07-10 16:37:44 +0300

    Added missing @Entity to StoredLocation.

--- src/main/java/x/mvmn/carpool/model/StoredLocation.java
+++ src/main/java/x/mvmn/carpool/model/StoredLocation.java
@@ -1,10 +1,12 @@
 package x.mvmn.carpool.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class StoredLocation {
 
 	protected int id;
