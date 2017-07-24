4e123c6629a485abdf4935cc4a3790d8935319dd
Mykola Makhin <mvmn.inbox@gmail.com>
2017-07-24 17:10:12 +0300

    Properly reload localization messages in case template caching is disabled (dev mode).

--- src/main/java/x/mvmn/carpool/l10n/ExtReloadableResourceBundleMessageSource.java
+++ src/main/java/x/mvmn/carpool/l10n/ExtReloadableResourceBundleMessageSource.java
@@ -1,5 +1,6 @@
 package x.mvmn.carpool.l10n;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
@@ -10,10 +11,24 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 public class ExtReloadableResourceBundleMessageSource extends ReloadableResourceBundleMessageSource {
 	public Map<String, String> getAll(Locale locale) {
 		Map<String, String> result = new TreeMap<>();
-		Properties props = this.getMergedProperties(locale).getProperties();
-		for (Object keyObj : props.keySet()) {
-			String key = keyObj.toString();
-			result.put(key, props.getProperty(key));
+		if (this.getCacheMillis() < 0) {
+			Properties props = this.getMergedProperties(locale).getProperties();
+			for (Object keyObj : props.keySet()) {
+				String key = keyObj.toString();
+				result.put(key, props.getProperty(key));
+			}
+		} else {
+			for (String basename : getBasenameSet()) {
+				List<String> filenames = calculateAllFilenames(basename, locale);
+				for (String filename : filenames) {
+					PropertiesHolder propHolder = getProperties(filename);
+					if (propHolder.getProperties() != null) {
+						for (Object key : propHolder.getProperties().keySet()) {
+							result.put(key.toString(), propHolder.getProperty(key.toString()));
+						}
+					}
+				}
+			}
 		}
 		return result;
 	}
