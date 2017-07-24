6c1bd3b06263e81f1b749fccd43913c54509b1c1
Mykola Makhin <mvmn.inbox@gmail.com>
2017-07-24 20:17:00 +0300

    Keep locale in separate cookie to avoid clearing of it on logout or session timeout.

--- src/main/java/x/mvmn/carpool/web/WebConfig.java
+++ src/main/java/x/mvmn/carpool/web/WebConfig.java
@@ -1,15 +1,34 @@
 package x.mvmn.carpool.web;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Value("${mvmncarpool.locales}")
+	String availableLocales;
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+		cookieLocaleResolver.setDefaultLocale(Locale.forLanguageTag(availableLocales.split("\\s*,\\s*")[0]));
+		cookieLocaleResolver.setCookieName("lang");
+		cookieLocaleResolver.setCookieMaxAge(12 * 30 * 24 * 60 * 60);
+		return cookieLocaleResolver;
 	}
 }
